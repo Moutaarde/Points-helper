9462e565987e4561810c1039160c13dd2f317ab5
Moutarde <goubarc@gmail.com>
2023-08-28 03:07:48 +0200

    ctk theme and more

--- program.py
+++ program.py
@@ -1,7 +1,7 @@
 import pyautogui
 from random import randint
-import PIL
 import time
+import customtkinter
 
 def get_point_search_bar():
     global search_bar_point
@@ -30,32 +30,39 @@ def searches():
         pyautogui.press('enter')
 
 def locate_activities():
+    activities_points = []
     activity_locations = list(pyautogui.locateAllOnScreen('reference_to_do.png'))
     if len(activity_locations) == 0:
-        return(0)
+        return(activities_points)
     else:
-        activities_points = []
         for location in activity_locations:
             activities_points.append(pyautogui.center(location))
-        return(1)
+    return(activities_points)
         
-def open_next_activity_or_scroll_down(activity_point):
-    if activity_point == 0:
+def open_next_activity_or_scroll_down(activities_points):
+    if activities_points == []:
         pyautogui.scroll(-450)
         return(450)
     else:
-        pyautogui.moveTo(activity_point.x, activity_point.y, 0.1*(1/speed))
-        pyautogui.click()
-        time.sleep(0.3*(1/speed))
-        pyautogui.hotkey('ctrl', 'w')
+        for x,y in activities_points:
+            pyautogui.moveTo(x, y, 0.2*(1/speed))
+            pyautogui.click()
         return(0)
+
+def get_which_activity():
+    return
+
+def do_activity():
+    return
 
 def make_activities():
     total_scroll = 0
     while total_scroll < 1800:
-        activity_point = locate_activity()
-        total_scroll = total_scroll + open_next_activity_or_scroll_down(activity_point)
+        activities_points = locate_activities()
+        total_scroll = total_scroll + open_next_activity_or_scroll_down(activities_points)
 
+
+screenWidth, screenHeight = pyautogui.size()
 rank = 2
 if rank==1:
     nb_searches = 60
@@ -66,4 +73,13 @@ speed = 1
 
 list_of_words=["about","all","also","and","as","at","be","because","but","by","can","come","could","day","do","even","find","first","for","from","get","give","go","have","he","her","here","him","his","how","if","in","into","it","its","just","know","like","look","make","man","many","me","more","my","new","no","not","now","of","on","one","only","or","other","our","out","people","say","see","she","so","some","take","tell","than","that","the","their","them","then","there","these","they","thing","think","this is an easter eag","those","time","to","twoup","use","very","want","way","we","well","what","when","which","who","will","with","would","year","you","your"]
 
-screenWidth, screenHeight = pyautogui.size()
+
+app = customtkinter.CTk()
+app.geometry("400x150")
+customtkinter.set_default_color_theme('theme.json')
+customtkinter.set_appearance_mode("dark")
+
+button = customtkinter.CTkButton(app, text="my button")
+button.pack(padx=20, pady=20)
+
+app.mainloop()
